92a324b2c63dff5bf82de55df004eba27cbbdfb1
Matt <91234966+mphDev84@users.noreply.github.com>
2022-05-02 16:53:49 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,11 +1,13 @@
 const myValue = "hello, this is a message!";
 console.log(myValue);
 
+//function to fetch MongoDB data from backend '/api' endpoint. 
 let request = async()=>{
     const response = await fetch ('http://localhost:3000/api')
     const data = await response.json();
     console.log('Data received is: ', data);
 
+    //display data on DOM
     for (let i=data.length-1;i>=0;i--){
         let myNameValue = data[i]["name"];
         let myRingsValue = data[i]["hasRings"];
